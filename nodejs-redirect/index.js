80f8ba55a53113aa7e98ec8e0a0cc76aea16df1a
Rat <33757638+RatDotRaw@users.noreply.github.com>
2023-12-15 13:27:28 +0100

    nodejs redirect

---
+++ nodejs-redirect/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.get('/*', (req, res) => {
+    res.redirect("https://ratdotraw.github.io")
+})
+
+app.listen(port, () => {
+    console.log(`nodejs listening on port ${port}`)
+})
